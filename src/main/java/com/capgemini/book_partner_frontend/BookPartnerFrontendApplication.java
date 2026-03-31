1850c2731c2a264378fd4588b495c8d60f1d29d2
prateekmishra-0 <mprateek580@gmail.com>
2026-03-31 11:35:46 +0530

    Initial commit: Spring Boot frontend skeleton

---
+++ src/main/java/com/capgemini/book_partner_frontend/BookPartnerFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.book_partner_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPartnerFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPartnerFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/book_partner_frontend/BookPartnerFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.book_partner_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPartnerFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
