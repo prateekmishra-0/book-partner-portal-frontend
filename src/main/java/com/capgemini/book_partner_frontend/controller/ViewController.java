cb8bdfb658afcbd344e4003e4202f292071c6fc2
prateekmishra-0 <mprateek580@gmail.com>
2026-03-31 15:26:20 +0530

    added the second page searching and all everything

---
+++ src/main/java/com/capgemini/book_partner_frontend/controller/ViewController.java
@@ -0,0 +1,16 @@
+package com.capgemini.book_partner_frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/employees")
+    public String showEmployeesPage() {
+        // Tells Spring to look in templates/employee/ for employees.html
+        return "employee/employees";
+    }
+
+
+}
